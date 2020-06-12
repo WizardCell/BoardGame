67f2b43644c88e027349e6052600d1a28b77da0a
Mohammad Mossa <mohammed.19.03@hotmail.com>
2020-06-13 02:49:35 +0300

    Untrack build files

--- build/CMakeFiles/TheMatrix.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for TheMatrix.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/doc/MtmEx3
-# Build directory: D:/doc/MtmEx3/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

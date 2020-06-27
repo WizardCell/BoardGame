60d91bd25a1ec8b5ce6bf8a060ebfd4ae3678fe0
Mohammad Mossa <mohammed.19.03@hotmail.com>
2020-06-27 06:31:31 +0300

    Character updated.
    Will start on board and the other characters today

--- MtmEx3/Character.cpp
+++ MtmEx3/Character.cpp
@@ -1,6 +1,40 @@
 
 #include "Character.h"
 
-mtm::Character::Character(int health, int ammo, int range, int power)
-	
+#include "Board.h"
+#include "Exception.h"
+
+mtm::Character::Character(units_t health, units_t ammo, units_t range, units_t power, Team team, CharacterType type)
+	: health(health), ammo(ammo), range(range), power(power), team(team), type(type)
+{
+	if (health <= 0 || ammo < 0 || range < 0 || power < 0)
+	{
+		throw IllegalArgument();
+	}
+}
+
+mtm::Character::Character(const Character& other)
+{
+	(*this) = other;
+}
+
+mtm::units_t mtm::Character::getHealth() const
+{
+	return health;
+}
+
+void mtm::Character::updateHealth(units_t delta)
+{
+	health += delta;
+}
+
+mtm::CharacterType mtm::Character::getType() const
+{
+	return type;
+}
+
+mtm::Team mtm::Character::getTeam() const
+{
+	return team;
+}
 
--- MtmEx3/Character.h
+++ MtmEx3/Character.h
@@ -3,6 +3,7 @@
 
 #include "Auxiliaries.h"
 
+
 namespace mtm
 {
 	class Character
@@ -13,17 +14,24 @@ namespace mtm
 		units_t range;
 		units_t power;
 		Team team;
-		CharacterType Type;
+		CharacterType type;
 
 	public:
-		Character(units_t health, units_t ammo, units_t range, units_t power)
-			: health(health), ammo(ammo), range(range), power(power) {}
+		Character(units_t health, units_t ammo, units_t range, units_t power, Team team, CharacterType type);
+		Character(const Character& other);
+		virtual ~Character() = default;
+		// @TODO: Board class to be implemented
+		//virtual void move(Board<Character>& board, const GridPoint& s_place, const GridPoint& d_place) = 0;
+		//virtual void attack(Board<Character>& board, const GridPoint& s_place,const GridPoint& d_place) = 0;
+
+		virtual void updateHealth(units_t delta);
+		virtual units_t getHealth() const;
+		CharacterType getType() const;
+		virtual Team getTeam() const;
+		virtual void reload() = 0;
+		virtual std::shared_ptr<Character> clone() const = 0;
 		
-		virtual ~Character() {}
-
-
-
-
+		
 
 
 	};
